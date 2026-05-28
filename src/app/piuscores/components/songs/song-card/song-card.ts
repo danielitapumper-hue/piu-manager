80517f07dfd749014f4f0d28f7fc6a4be488324a
DESKTOP-RC83VJH\danie <dmohamed@globons.com>
2026-05-28 00:23:19 -0300

    link a busqueda de youtube

--- src/app/piuscores/components/songs/song-card/song-card.ts
+++ src/app/piuscores/components/songs/song-card/song-card.ts
@@ -9,5 +9,14 @@ import { ImageSrcPipe } from '@piuscores/pipes/image-src-pipe';
   templateUrl: './song-card.html',
 })
 export class SongCard {
-  chartScore = input.required<ChartScore>()
+  chartScore = input.required<ChartScore>();
+
+  openYoutubeSearch() {
+    const song = this.chartScore().chart.song.name;
+    const shorthand = this.chartScore().chart.shorthand;
+    const query = `PUMP IT UP ${song} ${shorthand}`;
+    const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+
+    window.open(youtubeSearchUrl, '_blank', 'noopener,noreferrer');
+  }
 }
