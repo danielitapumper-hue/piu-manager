329cc29aa953db271f70ef27231dcc2b212a0c97
DESKTOP-RC83VJH\danie <dmohamed@globons.com>
2026-06-02 01:11:49 -0300

    Agrego filtros en scores-page

--- src/app/piuscores/pages/scores-page/scores-page.ts
+++ src/app/piuscores/pages/scores-page/scores-page.ts
@@ -1,19 +1,24 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, output, signal } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { SavedFilters } from "@piuscores/components/filters/saved-filters/saved-filters";
 import { LocalStorageService } from '@piuscores/services/local-storage-service';
 import { ChartScore } from '@piuscores/interfaces/chart-score';
 import { CategoryCharts } from '@piuscores/interfaces/category-charts';
 import { ImageSrcPipe } from '@piuscores/pipes/image-src-pipe';
+import { SongTypesFilter } from "@piuscores/components/filters/song-types-filter/song-types-filter";
+import { SearchByNameFilter } from "@piuscores/components/filters/search-by-name-filter/search-by-name-filter";
+import { PiuscoresService } from '@piuscores/services/piuscores-service';
 
 @Component({
   selector: 'app-scores-page',
-  imports: [SavedFilters, ImageSrcPipe, DecimalPipe],
+  imports: [SavedFilters, ImageSrcPipe, DecimalPipe, SongTypesFilter, SearchByNameFilter],
   templateUrl: './scores-page.html',
 })
 export class ScoresPage {
   localStorageService = inject(LocalStorageService);
+  piuScoresService = inject(PiuscoresService);
 
+  private songTypesFilter: boolean[] = [];
   private scoresList: ChartScore[] = [];
 
   scoresListByLetterGrade = signal<CategoryCharts[]>([]);
@@ -25,6 +30,7 @@ export class ScoresPage {
   searchLastFilter() {
     const lastFilter = this.localStorageService.lastFilter();
     if (lastFilter.filter) {
+      this.songTypesFilter = lastFilter.songTypes;
       this.scoresList = this.localStorageService.getTierListByScoresFromLocalStorage(
         this.localStorageService.searchFiltersToChartTypeLevelKey(lastFilter)
       );
@@ -32,9 +38,19 @@ export class ScoresPage {
     }
   }
 
+  searchBySongTypes(songTypes: boolean[]) {
+    this.songTypesFilter = songTypes;
+    this.scoresListByLetterGrade.set(this.getScoresListByLetterGrade());
+    this.localStorageService.setLocalStorageLastSongTypesFilter(songTypes);
+  }
+
   searchSavedFilter(savedFilter: string) {
     this.scoresList = this.localStorageService.getTierListByScoresFromLocalStorage(savedFilter);
     this.scoresListByLetterGrade.set(this.getScoresListByLetterGrade());
+  }
+
+  searchBySongName(songName: string) {
+    this.scoresListByLetterGrade.set(this.getScoresListByLetterGrade(songName));
   }
 
   private getScoresListByLetterGrade(songName?: string): CategoryCharts[] {
@@ -61,7 +77,9 @@ export class ScoresPage {
   }
 
   private getFilteredScoresList(songName?: string): ChartScore[] {
+    const songTypesFilter = this.piuScoresService.songTypes.filter((_, i) => this.songTypesFilter[i]);
     return this.scoresList.filter(item => item.score &&
+      songTypesFilter.includes(item.chart.song.type) &&
       (!songName || item.chart.song.name.toLowerCase().includes(songName.toLowerCase())));
   }
 
