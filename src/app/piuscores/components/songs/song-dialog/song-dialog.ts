32b5c25e80cd6cbda510677545134c0af8c4eced
DESKTOP-RC83VJH\danie <dmohamed@globons.com>
2026-06-03 21:01:07 -0300

    Agrego funcionalidad "upscore" cuando cargo un nuevo y mejor score

--- src/app/piuscores/components/songs/song-dialog/song-dialog.ts
+++ src/app/piuscores/components/songs/song-dialog/song-dialog.ts
@@ -1,24 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { ChartScore } from '@piuscores/interfaces/chart-score';
 import { Score } from '@piuscores/interfaces/score';
 import { ScoreData } from "../score-data/score-data";
 import { ScoreForm } from "../score-form/score-form";
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-song-dialog',
-  imports: [ScoreData, ScoreForm],
+  imports: [ScoreData, ScoreForm, DecimalPipe],
   templateUrl: './song-dialog.html',
 })
 export class SongDialog {
   dialogRef = inject<DialogRef<ChartScore | undefined>>(DialogRef<ChartScore | undefined>);
   chartScore = inject<ChartScore>(DIALOG_DATA);
 
+  newScore = signal<number>(0);
+
   closeDialog(): void {
     this.dialogRef.close(this.chartScore);
   }
 
   handleScoreSaved(updatedScore: Score | undefined): void {
+    if (updatedScore) {
+      const currentScore = this.chartScore.score?.score ?? 0;
+      if (updatedScore.score > currentScore) {
+        this.newScore.set(updatedScore.score - currentScore);
+      }
+      else {
+        this.newScore.set(0);
+      }
+    }
+
     this.chartScore = {
       ...this.chartScore,
       score: updatedScore,
