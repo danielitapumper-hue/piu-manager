944b3c8ef7756d6cee1728687e15d75f06f21396
DESKTOP-RC83VJH\danie <dmohamed@globons.com>
2026-06-14 17:14:12 -0300

    Separo en componente de procesamiento de imágenes

--- src/app/piuscores/components/navbar/navbar.ts
+++ src/app/piuscores/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '@auth/services/auth-service';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, UpperCasePipe],
   templateUrl: './navbar.html',
 })
 export class Navbar {
